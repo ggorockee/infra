6e2bbb5d15966e9d2a39e4d032aa940fcb4786f1
ggorockee <ggorockee@gmail.com>
2025-05-18 21:19:24 +0900

    output 삭제

--- aws/terraform/envs/dev/outputs.tf
+++ aws/terraform/envs/dev/outputs.tf
@@ -1,15 +1,15 @@
-output "vpc_id" {
-  description = "생성된 VPC ID"
-  value       = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   description = "생성된 VPC ID"
+#   value       = module.vpc.vpc_id
+# }
 
-output "public_subnet_ids" {
-  description = "퍼블릭 서브넷 ID 목록"
-  value       = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   description = "퍼블릭 서브넷 ID 목록"
+#   value       = module.vpc.public_subnet_ids
+# }
 
 
-output "private_subnet_ids" {
-  description = "퍼블릭 서브넷 ID 목록"
-  value       = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   description = "퍼블릭 서브넷 ID 목록"
+#   value       = module.vpc.private_subnet_ids
+# }
