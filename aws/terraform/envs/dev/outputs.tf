ad45c1b81be55afd953ad041867204d92bda6565
ggorockee <ggorockee@gmail.com>
2025-05-20 09:37:15 +0900

    eks 설치치

--- aws/terraform/envs/dev/outputs.tf
+++ aws/terraform/envs/dev/outputs.tf
@@ -30,135 +30,203 @@ output "ssm_vpc_endpoint_network_interface_ids" {
   value       = module.vpc-endpoint.ssm_vpc_endpoint_network_interface_ids
 }
 
-## EKS
-# output "eks_access_entries" {
-#   description = "Map of access entries created and their attributes"
-#   value       = module.eks.access_entries
-# }
+################################################### EKS
+output "eks_access_entries" {
+  description = "Map of access entries created and their attributes"
+  value       = module.eks.access_entries
+}
 
+output "eks_access_policy_associations" {
+  description = "Map of eks cluster access policy associations created and their attributes"
+  value       = module.eks.access_policy_associations
+}
 
+output "eks_cloudwatch_log_group_arn" {
+  description = "Arn of cloudwatch log group created"
+  value       = module.eks.cloudwatch_log_group_arn
+}
 
-# output "eks_access_policy_associations" {
-#   description = "Map of eks cluster access policy associations created and their attributes"
-#   value       = module.eks.access_policy_associations
-# }
+output "eks_cloudwatch_log_group_name" {
+  description = "Name of cloudwatch log group created"
+  value       = module.eks.cloudwatch_log_group_name
+}
 
+output "eks_cluster_addons" {
+  description = "Map of attribute maps for all EKS cluster addons enabled"
+  value       = module.eks.cluster_addons
+}
 
+output "eks_cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = module.eks.cluster_arn
+}
 
+output "eks_cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
 
-# output "eks_cloudwatch_log_group_arn" {
-#   description = "Arn of cloudwatch log group created"
-#   value       = module.eks.cloudwatch_log_group_arn
-# }
+output "eks_cluster_dualstack_oidc_issuer_url" {
+  description = "Dual-stack compatible URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_dualstack_oidc_issuer_url
+}
 
+output "eks_cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
 
+output "eks_cluster_iam_role_arn" {
+  description = "Cluster IAM role ARN"
+  value       = module.eks.cluster_iam_role_arn
+}
 
-# Description: Name of cloudwatch log group created
-# cluster_addons
+output "eks_cluster_iam_role_name" {
+  description = "Cluster IAM role name"
+  value       = module.eks.cluster_iam_role_name
+}
 
-# Description: Map of attribute maps for all EKS cluster addons enabled
-# cluster_arn
+output "eks_cluster_iam_role_unique_id" {
+  description = "Stable and unique string identifying the IAM role"
+  value       = module.eks.cluster_iam_role_unique_id
+}
 
-# Description: The Amazon Resource Name (ARN) of the cluster
-# cluster_certificate_authority_data
+output "eks_cluster_id" {
+  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
+  value       = module.eks.cluster_id
+}
 
-# Description: Base64 encoded certificate data required to communicate with the cluster
-# cluster_dualstack_oidc_issuer_url
+output "eks_cluster_identity_providers" {
+  description = "Map of attribute maps for all EKS identity providers enabled"
+  value       = module.eks.cluster_identity_providers
+}
 
-# Description: Dual-stack compatible URL on the EKS cluster for the OpenID Connect identity provider
-# cluster_endpoint
+output "eks_cluster_ip_family" {
+  description = "The IP family used by the cluster (e.g. `ipv4` or `ipv6`)"
+  value       = module.eks.cluster_ip_family
+}
 
-# Description: Endpoint for your Kubernetes API server
-# cluster_iam_role_arn
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
 
-# Description: Cluster IAM role ARN
-# cluster_iam_role_name
+output "eks_cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
 
-# Description: Cluster IAM role name
-# cluster_iam_role_unique_id
+output "eks_cluster_platform_version" {
+  description = "Platform version for the cluster"
+  value       = module.eks.cluster_platform_version
+}
 
-# Description: Stable and unique string identifying the IAM role
-# cluster_id
+output "eks_cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
+  value       = module.eks.cluster_primary_security_group_id
+}
 
-# Description: The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts
-# cluster_identity_providers
+output "eks_cluster_security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the cluster security group"
+  value       = module.eks.cluster_security_group_arn
+}
 
-# Description: Map of attribute maps for all EKS identity providers enabled
-# cluster_ip_family
+output "eks_cluster_security_group_id" {
+  description = "ID of the cluster security group"
+  value       = module.eks.cluster_security_group_id
+}
 
-# Description: The IP family used by the cluster (e.g. `ipv4` or `ipv6`)
-# cluster_name
+output "eks_cluster_service_cidr" {
+  description = "The CIDR block where Kubernetes pod and service IP addresses are assigned from"
+  value       = module.eks.cluster_service_cidr
+}
 
-# Description: The name of the EKS cluster
-# cluster_oidc_issuer_url
+output "eks_cluster_status" {
+  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
+  value       = module.eks.cluster_status
+}
 
-# Description: The URL on the EKS cluster for the OpenID Connect identity provider
-# cluster_platform_version
+output "eks_cluster_tls_certificate_sha1_fingerprint" {
+  description = "The SHA1 fingerprint of the public key of the cluster's certificate"
+  value       = module.eks.cluster_tls_certificate_sha1_fingerprint
+}
 
-# Description: Platform version for the cluster
-# cluster_primary_security_group_id
+output "eks_cluster_version" {
+  description = "The Kubernetes version for the cluster"
+  value       = module.eks.cluster_version
+}
 
-# Description: Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console
-# cluster_security_group_arn
+output "eks_eks_managed_node_groups" {
+  description = "Map of attribute maps for all EKS managed node groups created"
+  value       = module.eks.eks_managed_node_groups
+}
 
-# Description: Amazon Resource Name (ARN) of the cluster security group
-# cluster_security_group_id
+output "eks_eks_managed_node_groups_autoscaling_group_names" {
+  description = "List of the autoscaling group names created by EKS managed node groups"
+  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
+}
 
-# Description: ID of the cluster security group
-# cluster_service_cidr
+output "eks_fargate_profiles" {
+  description = "Map of attribute maps for all EKS Fargate Profiles created"
+  value       = module.eks.fargate_profiles
+}
 
-# Description: The CIDR block where Kubernetes pod and service IP addresses are assigned from
-# cluster_status
+output "eks_kms_key_arn" {
+  description = "The Amazon Resource Name (ARN) of the key"
+  value       = module.eks.kms_key_arn
+}
 
-# Description: Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`
-# cluster_tls_certificate_sha1_fingerprint
+output "eks_kms_key_id" {
+  description = "The globally unique identifier for the key"
+  value       = module.eks.kms_key_id
+}
 
-# Description: The SHA1 fingerprint of the public key of the cluster's certificate
-# cluster_version
+output "eks_kms_key_policy" {
+  description = "The IAM resource policy set on the key"
+  value       = module.eks.kms_key_policy
+}
 
-# Description: The Kubernetes version for the cluster
-# eks_managed_node_groups
+output "eks_node_iam_role_arn" {
+  description = "EKS Auto node IAM role ARN"
+  value       = module.eks.node_iam_role_arn
+}
 
-# Description: Map of attribute maps for all EKS managed node groups created
-# eks_managed_node_groups_autoscaling_group_names
+output "eks_node_iam_role_name" {
+  description = "EKS Auto node IAM role name"
+  value       = module.eks.node_iam_role_name
+}
 
-# Description: List of the autoscaling group names created by EKS managed node groups
-# fargate_profiles
+output "eks_node_iam_role_unique_id" {
+  description = "Stable and unique string identifying the IAM role"
+  value       = module.eks.node_iam_role_unique_id
+}
 
-# Description: Map of attribute maps for all EKS Fargate Profiles created
-# kms_key_arn
+output "eks_node_security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the node shared security group"
+  value       = module.eks.node_security_group_arn
+}
 
-# Description: The Amazon Resource Name (ARN) of the key
-# kms_key_id
+output "eks_node_security_group_id" {
+  description = "ID of the node shared security group"
+  value       = module.eks.node_security_group_id
+}
 
-# Description: The globally unique identifier for the key
-# kms_key_policy
+output "eks_oidc_provider" {
+  description = "The OpenID Connect identity provider (issuer URL without leading `https://`)"
+  value       = module.eks.oidc_provider
+}
 
-# Description: The IAM resource policy set on the key
-# node_iam_role_arn
+output "eks_oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider if `enable_irsa = true`"
+  value       = module.eks.oidc_provider_arn
+}
 
-# Description: EKS Auto node IAM role ARN
-# node_iam_role_name
+output "eks_self_managed_node_groups" {
+  description = "Map of attribute maps for all self managed node groups created"
+  value       = module.eks.self_managed_node_groups
+}
 
-# Description: EKS Auto node IAM role name
-# node_iam_role_unique_id
-
-# Description: Stable and unique string identifying the IAM role
-# node_security_group_arn
-
-# Description: Amazon Resource Name (ARN) of the node shared security group
-# node_security_group_id
-
-# Description: ID of the node shared security group
-# oidc_provider
-
-# Description: The OpenID Connect identity provider (issuer URL without leading `https://`)
-# oidc_provider_arn
-
-# Description: The ARN of the OIDC Provider if `enable_irsa = true`
-# self_managed_node_groups
-
-# Description: Map of attribute maps for all self managed node groups created
-# self_managed_node_groups_autoscaling_group_names
-
-# Description: List of the autoscaling group names created by self-managed node groups
+output "eks_self_managed_node_groups_autoscaling_group_names" {
+  description = "List of the autoscaling group names created by self-managed node groups"
+  value       = module.eks.self_managed_node_groups_autoscaling_group_names
+}
