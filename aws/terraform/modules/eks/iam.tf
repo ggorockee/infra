324589eb313201c197586d319bf0ae87529ca450
ggorockee <ggorockee@gmail.com>
2025-05-23 15:27:22 +0900

    eks update

--- aws/terraform/modules/eks/iam.tf
+++ aws/terraform/modules/eks/iam.tf
@@ -1,35 +1,35 @@
-// ─────────────────────────────────────────────────────────────
-// EKS 클러스터용 IAM Role 생성
-// ─────────────────────────────────────────────────────────────
+# // ─────────────────────────────────────────────────────────────
+# // EKS 클러스터용 IAM Role 생성
+# // ─────────────────────────────────────────────────────────────
 
-# 1) AssumeRole 정책 문서
+# # 1) AssumeRole 정책 문서
 
-data "aws_iam_policy_document" "eks_cluster_assume" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
+# data "aws_iam_policy_document" "eks_cluster_assume" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       type        = "Service"
+#       identifiers = ["eks.amazonaws.com"]
+#     }
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
-# IAM Role 생성
-resource "aws_iam_role" "eks_cluster" {
-  name               = "${var.cluster_name}-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
+# # IAM Role 생성
+# resource "aws_iam_role" "eks_cluster" {
+#   name               = "${var.cluster_name}-cluster-role"
+#   assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
 
-  tags = {
-    "Name" = "${var.cluster_name}-cluster-role"
-  }
-}
+#   tags = {
+#     "Name" = "${var.cluster_name}-cluster-role"
+#   }
+# }
 
-# 3) 필요한 AWS 관리형 정책 연결
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  for_each = {
-    for key, value in var.cluster_policies : key => value
-  }
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
-}
+# # 3) 필요한 AWS 관리형 정책 연결
+# resource "aws_iam_role_policy_attachment" "cluster_policy" {
+#   for_each = {
+#     for key, value in var.cluster_policies : key => value
+#   }
+#   role       = aws_iam_role.eks_cluster.name
+#   policy_arn = "arn:aws:iam::aws:policy/${each.value}"
+# }
--- aws/terraform/modules/eks/locals.tf
+++ aws/terraform/modules/eks/locals.tf
@@ -1,33 +1,38 @@
 locals {
+  # tag
+  tags = merge({
+    Managed_by = "Terraform"
+  }, var.tags)
+
   # API 엔드포인트 접근 제어
   cluster_endpoint_private_access = var.cluster_endpoint_private_access # true
   cluster_endpoint_public_access  = var.cluster_endpoint_public_access  # false
 
   # aws-auth ConfigMap 자동 관리 해제 (AssumeRole 방식)
-  manage_aws_auth = var.manage_aws_auth # false
+  # manage_aws_auth = var.manage_aws_auth # false
 
-  # IRSA/OIDC 사용
-  enable_irsa = var.enable_irsa # true
+  # # IRSA/OIDC 사용
+  # enable_irsa = var.enable_irsa # true
 
-  # CloudWatch Logs 비활성화
-  create_cloudwatch_log_group = var.create_cloudwatch_log_group # false
+  # # CloudWatch Logs 비활성화
+  # create_cloudwatch_log_group = var.create_cloudwatch_log_group # false
 
-  access_entries = {
-    cluster_assume_role = {
-      principal_arn = aws_iam_role.eks_cluster.arn
-      type          = "STANDARD"
-    }
-  }
+  # access_entries = {
+  #   cluster_assume_role = {
+  #     principal_arn = aws_iam_role.eks_cluster.arn
+  #     type          = "STANDARD"
+  #   }
+  # }
 
-  base_addons = var.cluster_addons
+  # base_addons = var.cluster_addons
 
-  csi_addon = length(var.ebs_csi_irsa_roles) > 0 ? {
-    "aws-ebs-csi-driver" = {
-      most_recent              = true
-      resolve_conflicts        = "PRESERVE"
-      service_account_role_arn = module.ebs_csi_irsa_role[keys(var.ebs_csi_irsa_roles)[0]].iam_role_arn
-    }
-  } : {}
+  # csi_addon = length(var.ebs_csi_irsa_roles) > 0 ? {
+  #   "aws-ebs-csi-driver" = {
+  #     most_recent              = true
+  #     resolve_conflicts        = "PRESERVE"
+  #     service_account_role_arn = module.ebs_csi_irsa_role[keys(var.ebs_csi_irsa_roles)[0]].iam_role_arn
+  #   }
+  # } : {}
 
-  cluster_addons = merge(local.base_addons, local.csi_addon)
+  # cluster_addons = merge(local.base_addons, local.csi_addon)
 }
