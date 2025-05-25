f7a00f5cacf4434d45416d1b94d25c48479fa0dc
ggorockee <ggorockee@gmail.com>
2025-05-25 11:01:57 +0900

    dat update

--- aws/terraform/envs/dev/data.tf
+++ aws/terraform/envs/dev/data.tf
@@ -1,20 +1,26 @@
-# VPC 정보 가져오기
 data "aws_vpc" "this" {
   id = data.terraform_remote_state.network.outputs.vpc_id
 }
 
-######
-# Current Region lookup
-######
-data "aws_region" "current" {
-  # no arguments needed
-}
+data "aws_region" "current" {}
 
 data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.this.id]
   }
+  filter {
+    name   = "tag:Name"
+    values = ["*private*"]
+  }
+}
+
+data "aws_route_tables" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+
   filter {
     name   = "tag:Name"
     values = ["*private*"]
