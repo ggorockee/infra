23d0c6374da44b13b9f6dff1532133fc46aa812f
ggorockee <ggorockee@gmail.com>
2025-05-25 10:15:01 +0900

    eks 삭제

--- aws/terraform/envs/dev/eks.tf
+++ aws/terraform/envs/dev/eks.tf
@@ -1,37 +1,37 @@
-module "eks" {
-  source = "../../modules/eks"
+#module "eks" {
+#  source = "../../modules/eks"
+#
+#  cluster_name    = "ggorockee-eks-cluster"
+#  cluster_version = "1.31"
+#
+#  cluster_endpoint_public_access  = false
+#  cluster_endpoint_private_access = true
+#
+#  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
+#  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
+#
+#  tags = {
+#    Environment = "dev"
+#    Terraform   = "true"
+#  }
+#
+#  eks_managed_node_groups = {
+#    GGOROCKEE_NODEGROUP = {
+#      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+#      ami_type        = "AL2_x86_64"
+#      instance_types  = ["t3.micro"]
+#      name            = "GGOROCKEE_NODEGROUP"
+#      use_name_prefix = false
+#      disk_size       = 20
+#      min_size        = 1
+#      max_size        = 2
+#      desired_size    = 1
+#    }
+#  }
+#
+#  using_nat = false
+#}
 
-  cluster_name    = "ggorockee-eks-cluster"
-  cluster_version = "1.31"
-
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
-
-  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-
-  eks_managed_node_groups = {
-    GGOROCKEE_NODEGROUP = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type        = "AL2_x86_64"
-      instance_types  = ["t3.micro"]
-      name            = "GGOROCKEE_NODEGROUP"
-      use_name_prefix = false
-      disk_size       = 20
-      min_size        = 1
-      max_size        = 2
-      desired_size    = 1
-    }
-  }
-
-  using_nat = false
-
-}
 #
 #
 #
