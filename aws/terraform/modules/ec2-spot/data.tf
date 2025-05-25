f7797240c91c560acd41c645c9244ba41a18ca40
ggorockee <ggorockee@gmail.com>
2025-05-25 19:38:06 +0900

    nat update

--- aws/terraform/modules/ec2-spot/data.tf
+++ aws/terraform/modules/ec2-spot/data.tf
@@ -31,7 +31,7 @@ data "aws_ami" "nat" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["amzn-ami-vpc-nat-hvm-*"]
+    values = ["amzn-ami-vpc-nat-*"]
   }
   owners = ["amazon"]
 }
