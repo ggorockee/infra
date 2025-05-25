842a39eec6cf23ac0834d2635285c45929ebf186
ggorockee <ggorockee@gmail.com>
2025-05-26 02:26:10 +0900

    update

--- aws/terraform/modules/eks/iam.tf
+++ aws/terraform/modules/eks/iam.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role" "eks_cluster" {
-  name               = local.cluster_role_name
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
-}
-
-resource "aws_iam_role" "eks_node" {
-  name               = local.node_group_role_name
-  assume_role_policy = data.aws_iam_policy_document.eks_node_assume.json
-}
+#resource "aws_iam_role" "eks_cluster" {
+#  name               = local.cluster_role_name
+#  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
+#}
+#
+#resource "aws_iam_role" "eks_node" {
+#  name               = local.node_group_role_name
+#  assume_role_policy = data.aws_iam_policy_document.eks_node_assume.json
+#}
