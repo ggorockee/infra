cbf3cfe86731d77c60cabadac67c844b87855504
ggorockee <ggorockee@gmail.com>
2025-05-26 15:25:03 +0900

    eks update

--- aws/terraform/modules/eks/iam.tf
+++ aws/terraform/modules/eks/iam.tf
@@ -19,3 +19,27 @@ resource "aws_iam_role_policy_attachment" "node_group" {
   role       = aws_iam_role.eks_node_group_role.name
   policy_arn = each.value
 }
+
+data "aws_iam_policy_document" "eks_assume" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = upper("${local.cluster_name}-cluster-role")
+  assume_role_policy = data.aws_iam_policy_document.eks_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policies" {
+  for_each   = toset(local.eks_cluster_policies)
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = each.value
+}
