5ca22c1dffae7158db929cb1826f4bab35009a4e
ggorockee <ggorockee@gmail.com>
2025-05-27 01:39:35 +0900

    로그 그룹 off

--- aws/terraform/modules/eks/main.tf
+++ aws/terraform/modules/eks/main.tf
@@ -21,6 +21,10 @@ module "eks" {
   vpc_id     = local.vpc_id
   subnet_ids = local.subnet_ids
 
+  create_kms_key              = false
+  cluster_encryption_config   = []
+  create_cloudwatch_log_group = false
+
   eks_managed_node_groups = {
     default = {
       min_size     = 1
