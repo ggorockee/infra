2afe96fa39033e40d918ebaecab622d28c5f2920
ggorockee <ggorockee@gmail.com>
2025-05-27 21:39:54 +0900

    eks 삭제

--- aws/terraform/envs/dev/eks.tf
+++ aws/terraform/envs/dev/eks.tf
@@ -1,12 +1,12 @@
-module "eks" {
-  source = "../../modules/eks"
+# module "eks" {
+#   source = "../../modules/eks"
 
-  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
-  tags = {
-    ManagedBy = "Terraform"
-  }
-  cluster_name    = "ggorockee-eks-cluster"
-  cluster_version = "1.32"
+#   vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
+#   subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
+#   tags = {
+#     ManagedBy = "Terraform"
+#   }
+#   cluster_name    = "ggorockee-eks-cluster"
+#   cluster_version = "1.32"
 
-}
+# }
