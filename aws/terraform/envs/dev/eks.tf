3771d42a91c33183a589e7402732c74525a3d1d6
ggorockee <ggorockee@gmail.com>
2025-05-27 23:31:58 +0900

    eks 정제

--- aws/terraform/envs/dev/eks.tf
+++ aws/terraform/envs/dev/eks.tf
@@ -1,12 +1,56 @@
-# module "eks" {
-#   source = "../../modules/eks"
+module "eks" {
+  source = "../../modules/eks"
 
-#   vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
-#   subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
-#   tags = {
-#     ManagedBy = "Terraform"
-#   }
-#   cluster_name    = "ggorockee-eks-cluster"
-#   cluster_version = "1.32"
+  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
+  tags = {
+    ManagedBy = "Terraform"
+  }
+  cluster_name    = "ggorockee-eks-cluster"
+  cluster_version = "1.32"
 
-# }
+
+  cluster_endpoint_public_access  = false
+  cluster_endpoint_private_access = true
+  create_kms_key                  = false
+  create_cloudwatch_log_group     = false
+
+  authentication_mode = "API"
+
+  access_entries = {}
+  #   cluster_admin = {
+  #     principal_arn = "arn:aws:iam::329599650491:user/ggorockee_saa_03"
+  #     policy_associations = {
+  #       admin_policy = {
+  #         policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  #         access_scope = { type = "cluster" }
+  #       }
+  #     }
+  #   }
+  # }
+
+  cluster_addons = {
+    eks-pod-identity-agent = {
+      disable = true
+    }
+
+    vpc-cni = {
+      disable = true
+    }
+  }
+
+  node_group_configs = {
+    use_name_prefix          = false
+    iam_role_use_name_prefix = false
+    min_size                 = 1
+    max_size                 = 1
+    desired_size             = 1
+    disk_size                = 10
+    instance_types           = ["t3.micro"]
+    capacity_type            = "SPOT"
+    labels                   = {}
+    taints                   = {}
+    tags                     = {}
+  }
+
+}
--- aws/terraform/modules/eks/locals.tf
+++ aws/terraform/modules/eks/locals.tf
@@ -1,35 +1,99 @@
 locals {
+  cluster_name                    = var.cluster_name
+  cluster_version                 = var.cluster_version
+  cluster_endpoint_public_access  = var.cluster_endpoint_public_access
+  cluster_endpoint_private_access = var.cluster_endpoint_private_access
+
+  authentication_mode         = var.authentication_mode
+  create_kms_key              = var.create_kms_key
+  cluster_encryption_config   = local.create_kms_key ? [] : []
+  create_cloudwatch_log_group = var.create_cloudwatch_log_group
+
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
-  tags            = merge({}, var.tags)
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  access_entries = {}
+  # {
+  #   cluster_admin = {
+  #     principal_arn = "arn:aws:iam::329599650491:user/ggorockee_saa_03"
+  #     policy_associations = {
+  #       admin_policy = {
+  #         policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  #         access_scope = { type = "cluster" }
+  #       }
+  #     }
+  #   }
+  # }
 
-  default_policies_ = [
-    "AmazonEKSWorkerNodePolicy",
-    "AmazonEKS_CNI_Policy",
-    "AmazonEC2ContainerRegistryReadOnly",
-  ]
+  base_cluster_addons = {
+    coredns                = { most_recent = true }
+    eks-pod-identity-agent = { most_recent = true }
+    kube-proxy             = { most_recent = true }
+    vpc-cni                = { most_recent = true }
+  }
 
-  combined_policies = concat(
-    local.default_policies_,
-    var.additional_default_policy
+  cluster_addons = merge(
+    local.base_cluster_addons,
+    var.cluster_addons
   )
 
-  default_policies = distinct(local.combined_policies)
-  attachments = flatten([
-    for ng_key, ng in module.eks.eks_managed_node_groups : [
-      for policy in local.default_policies : {
-        ng_key    = ng_key
-        policy    = policy
-        role_name = ng.iam_role_name
-      }
-    ]
-  ])
+  tags = merge({}, var.tags)
 
-  attachment_map = {
-    for att in local.attachments :
-    "${att.ng_key}-${att.policy}" => att
+
+
+
+
+
+
+
+
+  # ============= node group ===============
+  default_node_group_policies = [
+    "AmazonEKSWorkerNodePolicy",
+    "AmazonEKS_CNI_Policy",
+    "AmazonEC2ContainerRegistryReadOnly"
+  ]
+
+  additional_node_group_policies = var.additional_node_group_policies
+  combined_policies = concat(
+    local.default_node_group_policies,
+    local.additional_node_group_policies,
+  )
+
+  nodegroup_policies = distinct(local.combined_policies)
+
+  default_node_group_configs = {
+    create_iam_role          = false
+    iam_role_use_name_prefix = true
+    use_name_prefix          = true
+    labels                   = {}
+    taints                   = {}
+    tags                     = {}
+    ami_type                 = "AL2_x86_64"
+    disable_api_termination  = false
+    ebs_optimized            = true
+    enable_monitoring        = false
+  }
+
+  node_group_configs = {
+    for ng_key, ng in var.node_group_configs : ng_key => merge(local.default_node_group_configs, {
+      name                     = ng_key
+      use_name_prefix          = ng.use_name_prefix
+      create_iam_role          = ng.create_iam_role
+      iam_role_use_name_prefix = ng.iam_role_use_name_prefix
+      min_size                 = ng.min_size
+      max_size                 = ng.max_size
+      desired_size             = ng.desired_size
+      disk_size                = ng.disk_size
+      instance_types           = ng.instance_types
+      capacity_type            = ng.capacity_type
+      labels                   = ng.labels
+      taints                   = ng.taints
+      tags                     = ng.tags
+      ami_type                 = ng.ami_type
+      disable_api_termination  = ng.disable_api_termination
+      ebs_optimized            = ng.ebs_optimized
+      enable_monitoring        = ng.enable_monitoring
+    })
   }
 }
