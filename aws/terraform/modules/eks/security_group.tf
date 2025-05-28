2e29ba2a17b573560571b9100aa18086e2756509
ggorockee <ggorockee@gmail.com>
2025-05-28 23:22:29 +0900

    eks sg update

--- aws/terraform/modules/eks/security_group.tf
+++ aws/terraform/modules/eks/security_group.tf
@@ -13,7 +13,7 @@ resource "aws_security_group_rule" "allow_eks_api_from_my_ip" {
 }
 
 resource "aws_security_group_rule" "allow_traffic_from_vpn" {
-  for_each    = local.node_group_configs
+  for_each    = module.eks_managed_node_group
   description = "Allow EKS API server access from OPENVPN"
   type        = "ingress"
   from_port   = 0
@@ -21,7 +21,7 @@ resource "aws_security_group_rule" "allow_traffic_from_vpn" {
   protocol    = "-1"
 
   # Cluster security group
-  security_group_id = module.eks_managed_node_group[each.key].node_security_group_id
+  security_group_id = each.value.security_group_id
 
   # 허용하려는 소스 CIDR
   cidr_blocks = ["10.0.1.154/32"]
