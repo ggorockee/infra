a44562caa4e71c19bc88ce07ec8c5f180be2567c
ggorockee <ggorockee@gmail.com>
2025-05-28 11:35:35 +0900

    노드그룹삭제

--- aws/terraform/envs/dev/eks.tf
+++ aws/terraform/envs/dev/eks.tf
@@ -31,20 +31,20 @@ module "eks" {
 
   cluster_addons = {}
 
-  node_group_configs = {
-    ggorockee-default-node-group = {
-      use_name_prefix                 = false
-      iam_role_use_name_prefix        = false
-      min_size                        = 1
-      max_size                        = 1
-      desired_size                    = 1
-      disk_size                       = 10
-      instance_types                  = ["t3.micro"]
-      capacity_type                   = "SPOT"
-      labels                          = {}
-      taints                          = {}
-      tags                            = {}
-      launch_template_use_name_prefix = false
-    }
-  }
+  node_group_configs = {}
+  #   ggorockee-default-node-group = {
+  #     use_name_prefix                 = false
+  #     iam_role_use_name_prefix        = false
+  #     min_size                        = 1
+  #     max_size                        = 1
+  #     desired_size                    = 1
+  #     disk_size                       = 10
+  #     instance_types                  = ["t3.micro"]
+  #     capacity_type                   = "SPOT"
+  #     labels                          = {}
+  #     taints                          = {}
+  #     tags                            = {}
+  #     launch_template_use_name_prefix = false
+  #   }
+  # }
 }
