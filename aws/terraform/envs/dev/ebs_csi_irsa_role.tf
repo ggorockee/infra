3c44c2d46f5043c9517ea0d01848b7861548977d
ggorockee <ggorockee@gmail.com>
2025-05-29 11:26:54 +0900

    ebs_csi_irsa_role.tf

---
+++ aws/terraform/envs/dev/ebs_csi_irsa_role.tf
@@ -0,0 +1,34 @@
+data "aws_caller_identity" "current" {}
+data "aws_eks_cluster" "this" {
+  name = local.eks_cluster_name
+}
+data "aws_iam_openid_connect_provider" "eks_oidc" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_role" "ebs_csi_irsa" {
+  name = "${local.eks_cluster_name}-ebs-csi-irsa"
+  depends_on = [ module.eks ]
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = data.aws_iam_openid_connect_provider.eks_oidc.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          # issuer 호스트명(sub) + ":sub" 키를 정확히 맞춰 주세요
+          "${replace(data.aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_irsa.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
--- aws/terraform/envs/dev/locals.tf
+++ aws/terraform/envs/dev/locals.tf
@@ -2,5 +2,5 @@ locals {
   region                  = data.aws_region.current.name
   private_route_table_ids = data.aws_route_tables.private.ids
   sorted_public_subnets   = sort(data.terraform_remote_state.network.outputs.public_subnet_ids)
-
+  eks_cluster_name        = var.eks_cluster_name
 }
