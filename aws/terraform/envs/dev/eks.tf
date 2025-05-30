1a76ac82d3912d7896c8afe35c27da5461085bd8
ggorockee <ggorockee@gmail.com>
2025-05-30 11:12:40 +0900

    eks update

--- aws/terraform/envs/dev/eks.tf
+++ aws/terraform/envs/dev/eks.tf
@@ -36,9 +36,9 @@ module "eks" {
     ggorockee-default-node-group = {
       use_name_prefix                 = false
       iam_role_use_name_prefix        = false
-      min_size                        = 1
-      max_size                        = 1
-      desired_size                    = 1
+      min_size                        = 2
+      max_size                        = 2
+      desired_size                    = 2
       disk_size                       = 10
       instance_types                  = ["t3.small"]
       capacity_type                   = "SPOT"
